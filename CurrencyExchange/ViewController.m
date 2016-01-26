3f56d4ab8490108cfc31873b57334bcda2583646
Roman Stasiv <romanstasiv@macminissu7.training.local>
2016-01-26 15:08:56 +0200

    useless commit again

--- CurrencyExchange/ViewController.m
+++ CurrencyExchange/ViewController.m
@@ -19,7 +19,9 @@
     // Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning {
+- (void)didReceiveMemoryWarning
+{
+    int sadVar = 0;//completely useless
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
