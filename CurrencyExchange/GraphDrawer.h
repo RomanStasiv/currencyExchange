c014ea9f011005e6bf49d0fd165f49054904bb66
alex4eetah <alex4eetah@gmail.com>
2016-01-29 12:04:32 +0200

    delete commited code

--- CurrencyExchange/GraphDrawer.h
+++ CurrencyExchange/GraphDrawer.h
@@ -35,32 +35,5 @@
 @property (nonatomic, strong) NSArray *pointsOfEURAskCurve;
 
 - (void)drawLineFromPointA:(CGPoint)a toPointB:(CGPoint)b WithWidth:(CGFloat)width andColor:(UIColor *)color;
-/*
-
-@property (nonatomic, strong) NSMutableArray *USDArray;
-@property (nonatomic, strong) NSMutableArray *EURArray;
-
-@property (nonatomic, strong) UIColor *USDStrokeColor;
-@property (nonatomic, strong) UIColor *EURStrokeColor;
-
-@property (nonatomic, assign) CGFloat segmentWidth;
-@property (nonatomic, assign) NSUInteger segmentWidthCount;
-@property (nonatomic, assign) CGFloat segmentHeight;
-@property (nonatomic, assign) NSUInteger segmentHeightCount;
-
-@property (nonatomic, assign) NSUInteger maxYvalue;
-@property (nonatomic, assign) NSUInteger minYvalue;
-
-@property (nonatomic, strong) NSArray *pointsOfUSDCurve;
-@property (nonatomic, strong) NSArray *pointsOfEURCurve;
-
-@property (nonatomic, assign) CGRect insetFrame;
-@property (nonatomic, assign) CGFloat inset;
-
-- (void)drawLineFromPointA:(CGPoint)a toPointB:(CGPoint)b WithWidth:(CGFloat)width andColor:(UIColor *)color;
-- (void)configureVariable;
-- (void)drawGrid;
-- (void)drawGraphForCurrency:(NSString *)currency;
-- (void)drawAxis;*/
 
 @end
