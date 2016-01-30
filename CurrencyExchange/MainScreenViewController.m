f648e919bcdf1956f49aaf47f023fc320c12f0bd
Vitaliy Yarkun <AdamFox@mbp-vitaliy.training.local>
2016-01-30 15:05:02 +0200

    Test class currency fix commit

--- CurrencyExchange/MainScreenViewController.m
+++ CurrencyExchange/MainScreenViewController.m
@@ -21,12 +21,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    //JSONParseCoreDataSave* workObject = [[JSONParseCoreDataSave alloc] init];
-    //TestCoreData* testObject = [[TestCoreData alloc] init];
+    JSONParseCoreDataSave* workObject = [[JSONParseCoreDataSave alloc] init];
+    TestCoreData* testObject = [[TestCoreData alloc] init];
     //Fetcher*tmp = [[Fetcher alloc]init];
     
     //[workObject JSONParse];
-    //[workObject deleteAllObjectsFromCoreData];
+    [workObject deleteAllObjectsFromCoreData];
     //[workObject loadCoreDataObjects];
     //[testObject insertFakeDataToCoreData];
     //[tmp averageCurrencyRate];
