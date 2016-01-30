1819895fa3162e5c655b99959d234062024b97de
alex4eetah <alex4eetah@gmail.com>
2016-01-31 00:01:37 +0200

    tryToAjust+

---
+++ CurrencyExchange/EarnNotificationView.h
@@ -0,0 +1,13 @@
+//
+//  EarnNotificationView.h
+//  CurrencyExchange
+//
+//  Created by alex4eetah on 1/30/16.
+//  Copyright © 2016 Roman Stasiv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EarnNotificationView : UIView
+
+@end
---
+++ CurrencyExchange/EarnNotificationView.m
@@ -0,0 +1,21 @@
+//
+//  EarnNotificationView.m
+//  CurrencyExchange
+//
+//  Created by alex4eetah on 1/30/16.
+//  Copyright © 2016 Roman Stasiv. All rights reserved.
+//
+
+#import "EarnNotificationView.h"
+
+@implementation EarnNotificationView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
