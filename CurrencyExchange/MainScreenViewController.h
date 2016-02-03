f61a51537840fa827d9ca77c95a5b49d2ac7854a
Melany <melany@macminissu9.training.local>
2016-02-03 11:39:17 +0200

    NSWeakTimer

--- CurrencyExchange/MainScreenViewController.h
+++ CurrencyExchange/MainScreenViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 #import "CurencyCourseGraph.h"
+#import "MSWeakTimer.h"
 
 @interface MainScreenViewController : UIViewController
 
--- CurrencyExchange/MainScreenViewController.m
+++ CurrencyExchange/MainScreenViewController.m
@@ -11,8 +11,15 @@
 #import "TestCoreData.h"
 #import "Fetcher.h"
 
+static const char *MainScreenViewControllerTimerQueueContext = "MainScreenViewControllerTimerQueueContext";
+
 @interface MainScreenViewController ()
 @property (weak, nonatomic) IBOutlet UIView *graph;
+
+@property (strong, nonatomic) MSWeakTimer *timer;
+@property (strong, nonatomic) MSWeakTimer *backgroundTimer;
+@property (strong, nonatomic) JSONParseCoreDataSave * workObject;
+@property (strong, nonatomic) dispatch_queue_t privateQueue;
 
 @end
 
@@ -21,14 +28,14 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    JSONParseCoreDataSave* workObject = [[JSONParseCoreDataSave alloc] init];
-    //TestCoreData* testObject = [[TestCoreData alloc] init];
+    self.workObject = [[JSONParseCoreDataSave alloc] init];
+    TestCoreData* testObject = [[TestCoreData alloc] init];
     Fetcher*tmp = [[Fetcher alloc]init];
     
-     NSLog(@"Documents Directory: %@", [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject]);
+     //NSLog(@"Documents Directory: %@", [[[NSFileManager defaultManager] URLsForDirectory:NSDocumentDirectory inDomains:NSUserDomainMask] lastObject]);
     
-    //[workObject deleteAllObjectsFromCoreData];
-    [workObject JSONParse];
+    //[self.workObject deleteAllObjectsFromCoreData];
+    //[self.workObject JSONParse];
     //[workObject loadCoreDataObjects];
     [tmp allBanksQuantity];
     //[testObject insertFakeDataToCoreData];
@@ -39,12 +46,57 @@
                                                   selector: @selector(JSONParse)
                                                   userInfo: nil
                                                    repeats: YES];*/
+    
+    self.timer = [MSWeakTimer scheduledTimerWithTimeInterval:10
+                                                      target:self.workObject
+                                                    selector:@selector(JSONParse)
+                                                    userInfo:nil
+                                                     repeats:YES
+                                               dispatchQueue:dispatch_get_main_queue()];
+
+}
+
+- (id)init
+{
+    if ((self = [super init]))
+    {
+        self.privateQueue = dispatch_queue_create("com.mindsnacks.private_queue", DISPATCH_QUEUE_CONCURRENT);
+        
+        self.backgroundTimer = [MSWeakTimer scheduledTimerWithTimeInterval:0.2
+                                                                    target:self.workObject
+                                                                  selector:@selector(JSONParse)
+                                                                  userInfo:nil
+                                                                   repeats:YES
+                                                             dispatchQueue:self.privateQueue];
+        
+        dispatch_queue_set_specific(self.privateQueue, (__bridge const void *)(self), (void *)MainScreenViewControllerTimerQueueContext, NULL);
+    }
+    
+    return self;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - MSWeakTimerDelegate
+
+- (void)mainThreadTimerDidFire:(MSWeakTimer *)timer
+{
+    NSAssert([NSThread isMainThread], @"This should be called from the main thread");
+    
+   }
+
+#pragma mark -
+
+- (void)backgroundTimerDidFire
+{
+    NSAssert(![NSThread isMainThread], @"This shouldn't be called from the main thread");
+    
+    const BOOL calledInPrivateQueue = dispatch_queue_get_specific(self.privateQueue, (__bridge const void *)(self)) == MainScreenViewControllerTimerQueueContext;
+    NSAssert(calledInPrivateQueue, @"This should be called on the provided queue");
 }
 
 @end
