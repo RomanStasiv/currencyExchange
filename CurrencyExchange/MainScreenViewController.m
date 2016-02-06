151b1c97c1755ca3551c12290809aa3163d9c966
Melany <melany@macminissu9.training.local>
2016-02-06 22:06:05 +0200

    useless

--- CurrencyExchange/MainScreenViewController.m
+++ CurrencyExchange/MainScreenViewController.m
@@ -66,9 +66,9 @@
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated
+    
 }
-
 #pragma mark - Notifications
 
 - (void) dealloc
